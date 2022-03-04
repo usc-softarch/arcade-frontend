bf4af0e3825ad9b54580401961aabb505e4b0ec3
Yojairo Morales <yojairom@usc.edu>
2022-03-04 15:17:33 -0800

    navbar update

--- src/ArcadeBar.js
+++ src/ArcadeBar.js
@@ -1,4 +1,5 @@
 import {AppBar, Box, Toolbar, Typography} from "@mui/material";
+import Button from "@mui/material/Button";
 
 function ArcadeBar() {
     return (
@@ -8,6 +9,11 @@ function ArcadeBar() {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         ARCADE
                     </Typography>
+                    <Button color="inherit">Home</Button>
+                    <Button color="inherit">Arch Recovery</Button>
+                    <Button color="inherit">Smell Detection</Button>
+                    <Button color="inherit">Metrics</Button>
+                    <Button color="inherit">Visualization</Button>
                 </Toolbar>
             </AppBar>
         </Box>
