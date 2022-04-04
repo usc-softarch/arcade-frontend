7a807e9f40a4c314d48d441e9f34e61df3856c8a
Gordon Su <gssu@usc.edu>
2022-04-03 19:15:43 -0700

    spring boot setup

---
+++ src/test/java/uscsoftarch/arcade/ArcadeApplicationTests.java
@@ -0,0 +1,13 @@
+package uscsoftarch.arcade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
