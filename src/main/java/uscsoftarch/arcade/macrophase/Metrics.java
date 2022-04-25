8bdb4808413ef170cb83a73726cee5079634269c
Dongchen Zou <zoud@usc.edu>
2022-04-25 12:32:36 -0700

    Capture output

--- src/main/java/uscsoftarch/arcade/macrophase/Metrics.java
+++ src/main/java/uscsoftarch/arcade/macrophase/Metrics.java
@@ -4,13 +4,29 @@ import edu.usc.softarch.arcade.decay.BatchDecayMetricsAnalyzer;
 import edu.usc.softarch.arcade.metrics.BatchSystemEvo;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Metrics {
+
+    private final Path root = Paths.get("Metrics");
     public Metrics(){
+        try {
+            Files.createDirectory(root);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not initialize folder for metrics!");
+        }
     }
 
     public void Run(String method, String dependencyRSF, String clusterRSF){
         try {
+            PrintStream OriginalOut = System.out;
+            PrintStream fileOut = new PrintStream("./Metrics/metrics.txt");
+
+            System.setOut(fileOut);
             if (method.equals("A2a")) {
                 BatchSystemEvo.main(new String[]{
                         clusterRSF
@@ -21,7 +37,8 @@ public class Metrics {
                         clusterRSF
                 });
             }
-            //All the output is in System.out, I am not sure how to handle that
+
+            System.setOut(OriginalOut);
         } catch (FileNotFoundException fnfe){
             throw new RuntimeException("Unable to run Metrics!");
         }
--- src/main/java/uscsoftarch/arcade/macrophase/SmellDetection.java
+++ src/main/java/uscsoftarch/arcade/macrophase/SmellDetection.java
@@ -3,18 +3,34 @@ package uscsoftarch.arcade.macrophase;
 import edu.usc.softarch.arcade.antipattern.detection.ArchSmellDetector;
 
 import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class SmellDetection {
+
+    private final Path root = Paths.get("Smells");
+
     public SmellDetection(){
+        try {
+            Files.createDirectory(root);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not initialize folder for smell detection!");
+        }
     }
 
     public void Run(String dependencyRSF,String clusterRSF, String serFiles, String mallet, Boolean isArc){
         String isArc_s= isArc? "true" : "false";
         try{
+            PrintStream OriginalOut = System.out;
+            PrintStream fileOut = new PrintStream("./Metrics/smells.txt");
+
             ArchSmellDetector.main(new String[]{
                     dependencyRSF,clusterRSF,serFiles,mallet,isArc_s
             });
-            //All the output is in System.out, I am not sure how to give them back
+
+            System.setOut(OriginalOut);
         } catch (IOException e) {
             throw new RuntimeException("Unable to run Smell Detection!");
         }
